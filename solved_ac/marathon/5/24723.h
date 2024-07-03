fb77cca70b9cb14479a36343b202cb601bfd52ec
ImHyuk <74406312+asimuleo@users.noreply.github.com>
2024-07-03 13:46:35 +0900

    마라톤 코스 5-A

---
+++ solved_ac/marathon/5/24723.h
@@ -0,0 +1,19 @@
+//
+// Created by 임혁 on 2024. 7. 3.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+
+using namespace std;
+
+
+void p_24723() {
+    int n;
+	cin >> n;
+
+	cout << pow(2, n);
+}
