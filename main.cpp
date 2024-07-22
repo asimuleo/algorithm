70957884cc0a9c5afa51f6e8def5012f955c1426
임혁 <cs.javah@kakao.com>
2024-07-22 23:02:02 +0900

    7주차 B~E

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 //#include "문제해결전략/9_15.h"
 //#include "algospot/blockgame.h"
 //#include "week-1/4375.h"
-// #include "solved_ac/marathon/6/1812.h"
+ #include "solved_ac/marathon/7/2816.h"
 // #include "swea/d4/장훈이의높은선반.h"
- #include "큰돌/week-3/13913.h"
+// #include "큰돌/week-3/2756.h"
 //#include "백준/브론즈/29863.h"
 
 
@@ -18,7 +18,7 @@ int main() {
 
 //    solve();
 
-    p_13913();
+    p_2816();
 
 //    blockgame();
 
