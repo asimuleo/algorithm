25316576b4fade3a59093fae098f16a541320fbc
임혁 <cs.javah@kakao.com>
2024-08-09 10:22:26 +0900

    공유기 설치 이진 탐색

--- 백준/골드/2110.h
+++ 백준/골드/2110.h
@@ -22,27 +22,23 @@ int N, C;
 vector<int> board;
 
 
-//
-bool play(int current, int last) {
-    // 채워야 하는 수가 남은 수 보다 크다면 불가
-    if (C - 2 > N - last) return false;
-
-    for (int i = 2; i < C; ++i) {
-        // last 보다 큰 인덱스 중에서 current 보다 큰 걸 찾는다.
-        int j = last + 1;
-        while (j < N) {
-            if (board[j] - board[last] >= current) {
-                break;
-            }
-            j++;
+// 가장 인접한 두 공유기의 거리가 주어졌을 때, dis (즉 가장 짧은)
+// 설치 가능한 공유기의 개수가 C 개 이상이면 true
+// 설치 가능한 공유기의 개수가 C 개 미만이면 false;
+// true -> dis 가 더 커도 공유기 C 개를 설치할 가능성이 있지 않을까?
+// false -> 현재 dis 에서는 공유기를 C 개 설치하는 것이 불가능 하다. dis 를 더 줄어야 한다.
+bool play(const int dis) {
+    int cnt = 1; // 설치한 공유기 개수 ( 첫 번째 공유기 )
+    int start = 0; // 순서상 가장 뒤에 있는 공유기 인덱스 ( 첫 번째 공유기 )
+    for (int i = 1; i < board.size(); ++i) {
+        // start 부터 i 까지의 거리가 dis 보다 이상이면 설치 가능하다.
+        // dis 가 가장 작다고 가정했기 때문이다.
+        if (board[i] - board[start] >= dis) {
+            start = i;
+            cnt++;
         }
-        if (j == N) {
-            // 못 찾았다면
-            return false;
-        }
-        last = j;
     }
-    return true;
+    return cnt >= C;
 }
 
 void p_2110() {
@@ -55,17 +51,42 @@ void p_2110() {
 
     sort(board.begin(), board.end());
 
-    int dis = N - 1;
+    // 최적의 dis 를 설정해야 하는데, 그냥 큰 것 부터 작아 질 때까지 하면 되지 않을까?
+    // 가능한 dis 중 가능 큰 값은 당연히 board.back() - board.front();
 
-    while (true) {
-        for (int i = 0; i < N - dis; ++i) {
-            // 2 개를 구한다.
-            int current = board[i + dis] - board[i];
-            if (play(current, i + dis)) {
-                cout << current;
-                return;
-            }
+    /**
+     *시간초과
+     */
+
+    // 완탐
+    // for (int i = board.back() - board.front(); i >= 0; --i) {
+    //     if(play(i)) { // 공유기 설치 가능하다면?
+    //         // 현재 dis 가 답이다.
+    //         cout << i;
+    //         return;
+    //     }
+    // }
+
+
+    int ret = 1;
+
+    // 절반 씩 줄여나가
+    int s = 1; // 공유기 최소 거리 1
+    int e = board.back() - board.front(); // 공유기 최대 거리
+
+    while (s <= e) {
+        const int dis = (s + e) / 2;
+        if (play(dis)) {
+            // 공유기 설치 성공!!!
+            ret = dis; // 성공했으니까 결과에 저장.
+            // dis 가 더 커도 공유기를 더 설치 할 수 있는 가능성을 체크해보자.
+            s = dis + 1;
+        } else {
+            // 공유기 설치 실패!!!
+            // dis 가 더 작아야 공유기 설치가 가능하다.
+            e = dis - 1;
         }
-        dis--;
     }
+
+    cout << ret;
 }
