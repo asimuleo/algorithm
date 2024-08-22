d97a78c7538f36cbe83fdaa5be4d70ae9e678618
임혁 <cs.javah@kakao.com>
2024-08-22 09:22:14 +0900

    메인 함수 정리

--- main.cpp
+++ main.cpp
@@ -2,30 +2,16 @@
 // Created by 임혁 on 2024. 5. 23.
 //
 
-//#include "swexpertacademy.h"
 //#include "문제해결전략/9_15.h"
 //#include "algospot/blockgame.h"
 //#include "week-1/4375.h"
-#include "solved_ac/marathon/9/2355.h"
-// #include "swea/d4/장훈이의높은선반.h"
-// #include "큰돌/week-3/3197_2.h"
- // #include "백준/골드/1753.h"
-
-
+//#include "solved_ac/marathon/9/2355.h"
+//#include "swea/d4/장훈이의높은선반.h"
+// #include "큰돌/week-3/9934.h"
+// #include "백준/골드/2091.h"
 //#include "solved_ac/marathon/3/3049.h"
 
 int main() {
 
-//    solve();
-
-
-
-    p_2355();
-
-//    blockgame();
-
-    // 장훈이의높은선반();
-
     return 0;
-
 }
